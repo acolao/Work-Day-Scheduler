701b91e0ca2dff8bfe7d98e66f1b36806fb1d922
Alison Colao <alison.siwek@gmail.com>
2023-08-21 14:07:52 -0700

    added background colors and hourupdater

--- script1.js
+++ script1.js
@@ -1,22 +1,44 @@
-$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+$("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
 $(document).ready(function () {
-    $('.saveBtn').on('click', function(){
-    let value = $(this).
-    siblings('.description').val();
-    let time = $(this).parent().attr('id');
+  $(".saveBtn").on('click', function () {
+    let value = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
 
-localStorage.setItem(time, value);
-console.log(value);
+    localStorage.setItem(time, value);
+    console.log(value);
 
-$('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-1 .description').val(localStorage.getItem('hour-1'));
-  $('#hour-2 .description').val(localStorage.getItem('hour-2'));
-  $('#hour-3 .description').val(localStorage.getItem('hour-3'));
-  $('#hour-4 .description').val(localStorage.getItem('hour-4'));
-  $('#hour-5 .description').val(localStorage.getItem('hour-5'));
-    })
+    function hourUpdater() {
+      let currentHour = dayjs().hour();
+
+      $(".time-block").each(function () {
+        let blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+
+    hourUpdater();
+
+    setInterval(hourUpdater, 15000);
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  });
 });
